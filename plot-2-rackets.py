2e108d0393eb737635e76bc493534c58480ca740
Liam Boreback <boreback.work@gmail.com>
2025-06-05 10:15:10 +0200

    ignore epoch time

--- plot-2-rackets.py
+++ plot-2-rackets.py
@@ -1,24 +1,24 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import io
 
-# Change this to the path of your CSV file
+def clean_csv_file(path, max_columns=7):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    cleaned_lines = [line for line in lines if len(line.strip().split(',')) <= max_columns]
+    return pd.read_csv(io.StringIO(''.join(cleaned_lines)))
+
+# File paths from command line arguments
 file1 = sys.argv[1]
 file2 = sys.argv[2]
 
-# Read the CSV file into a pandas DataFrame
-data1 = pd.read_csv(file1)
-data2 = pd.read_csv(file2)
+# Read the cleaned CSV files
+data1 = clean_csv_file(file1)
+data2 = clean_csv_file(file2)
 
-# Print the DataFrame columns for verification
+# Print columns for verification
 print("Columns in CSV:", data1.columns)
-
-# Determine if there's a 'time' column; otherwise create one
-if 'time' not in data1.columns:
-    sample_rate = 1000  # Adjust this to your sensor's sample rate
-    data1['time'] = data1.index / sample_rate
-    data2['time'] = data2.index / sample_rate
-
 
 # Group accelerometer and gyroscope columns
 accel_columns = ['ax', 'ay', 'az']
@@ -29,20 +29,20 @@ fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(14, 10))
 
 for i, col in enumerate(accel_columns):
     ax = axes[i][0]
-    ax.plot(data1['time'], data1[col], label=f'File 1')
-    ax.plot(data2['time'], data2[col], label=f'File 2', linestyle='--')
+    ax.plot(data1.index, data1[col], label='File 1')
+    ax.plot(data2.index, data2[col], label='File 2', linestyle='--')
     ax.set_title(f"Accelerometer {col[1].upper()} Axis")
-    ax.set_xlabel("Time (s)")
+    ax.set_xlabel("Sample Index")
     ax.set_ylabel(f"{col} (m/s^2)")
     ax.grid(True)
     ax.legend()
 
 for i, col in enumerate(gyro_columns):
     ax = axes[i][1]
-    ax.plot(data1['time'], data1[col], label=f'File 1')
-    ax.plot(data2['time'], data2[col], label=f'File 2', linestyle='--')
+    ax.plot(data1.index, data1[col], label='File 1')
+    ax.plot(data2.index, data2[col], label='File 2', linestyle='--')
     ax.set_title(f"Gyroscope {col[1].upper()} Axis")
-    ax.set_xlabel("Time (s)")
+    ax.set_xlabel("Sample Index")
     ax.set_ylabel(f"{col} (deg/s)")
     ax.grid(True)
     ax.legend()
